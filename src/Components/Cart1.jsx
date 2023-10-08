7b02bd43051f3c65abba9bebb4fd8262055d70b0
sakirmohammad775 <safayetsakirmohammad@gmail.com>
2023-10-09 01:06:38 +0600

    try to added a photo from image BB

--- src/Components/Cart1.jsx
+++ src/Components/Cart1.jsx
@@ -5,7 +5,7 @@ const Cart1 = () => {
         <div className="my-16">
             <div className="hero bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <img src="src\assets\5eea8a43988ee356890d7de3.webp" className="max-w-sm rounded-lg shadow-2xl" />
+                <img src="https://i.ibb.co/xYwgz3Z/5eea8a43988ee356890d7de3.webp" className="max-w-sm rounded-lg shadow-2xl" />
             <div>
             <h1 className="text-5xl font-bold">Walking</h1>
             <p className="py-6">Walking is a low-impact aerobic exercise that promotes cardiovascular health, improves muscle tone, and boosts overall well-being. It is accessible, simple, and an effective way to stay active.</p>
