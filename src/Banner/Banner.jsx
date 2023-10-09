63ae6acc1369d41b203e5686fcdfa4d359e5ccb4
sakirmohammad775 <safayetsakirmohammad@gmail.com>
2023-10-09 23:25:26 +0600

    auth provider added thing

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div>
             <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/pQSw97S/frontpicture.jpg)'}}>
             </div>
-            <img src="src\assets\frontpicture.jpg" alt="" />
+        
                 
         </div>
     );
